e5c652baf5c47a2514827c5eeaf67e106b50491d
Alexander S Padalitsa <padalitsa@gmail.com>
2026-09-18 08:49:32 +0700

    33

--- Практикум 1/33.py
+++ Практикум 1/33.py
@@ -3,8 +3,13 @@ n = int(input())
 s = int(input())
 num = int(input())
 page = n * s
-st = n // s
-page_num = ceil(num / page)
-num_st = ceil((num % page) / n)
-num_str = ceil((num % page) % n)
+if num % page == 0:
+    page_num = num // page
+    num_st = s
+    num_str = n
+else:
+    page_num = ceil(num / page)
+    num_st = ceil((num % page) / n)
+    num_str = ceil((num % page) % n)
+
 print(f'странца {page_num} столбец {num_st} строка {num_str}')
